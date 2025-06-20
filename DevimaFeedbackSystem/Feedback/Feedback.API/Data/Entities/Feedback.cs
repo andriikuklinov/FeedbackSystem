e1236e5d05f23011278afce07b5c5ec800ba049a
andriikuklinov <akuklinov@outlook.com>
2025-06-21 00:53:34 +0300

    Add feedback domain model.

---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Data/Entities/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using DevimaFeedbackSystem.Common.Core.DataAccess.Model;
+
+namespace Feedback.API.Data.Entities
+{
+    public class Feedback : IEntity
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int Rating { get; set; }
+        public int ModuleId { get; set; }
+        public int UserId { get; set; }
+    }
+}
