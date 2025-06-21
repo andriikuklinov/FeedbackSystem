dbd7a19428f81dcb9b1b692df74c64d5b8666311
andriikuklinov <akuklinov@outlook.com>
2025-06-21 17:20:27 +0300

    Configure Grpc.

--- DevimaFeedbackSystem/Feedback/Feedback.API/Program.cs
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Program.cs
@@ -1,4 +1,5 @@
 using Feedback.API.Data;
+using Feedback.API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,9 @@ builder.Services.AddDbContext<FeedbackContext>((options) =>
 {
     options.UseInMemoryDatabase("FeedbackDb");
 });
+builder.Services.AddGrpc();
 
 var app = builder.Build();
-
-
+app.MapGrpcService<FeedbackService>();
 
 app.Run();
