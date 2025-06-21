94a58354d63c37301e36135e4d4db28cba28a7c2
andriikuklinov <akuklinov@outlook.com>
2025-06-21 18:24:01 +0300

    Implement FeedbackService.

--- DevimaFeedbackSystem/Feedback/Feedback.API/Services/FeedbackService.cs
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Services/FeedbackService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
-using Feedback.API.Data.Entities;
+using Feedback.API.Feedbacks.Commands;
 using Feedback.API.Protos;
+using Grpc.Core;
+using MediatR;
+using FeedbackEntity = Feedback.API.Data.Entities.Feedback;
 
 namespace Feedback.API.Services
 {
@@ -8,6 +11,22 @@ namespace Feedback.API.Services
     {
         private readonly ILogger<FeedbackService> _logger;
         private readonly IMapper _mapper;
-        private readonly IFeedbackService
+        private readonly ISender _sender;
+
+        public FeedbackService(IMapper mapper, ILogger<FeedbackService> logger, ISender sender)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _sender = sender;
+        }
+
+        public override async Task<FeedbackModel> CreateFeedback(CreateFeedbackRequest request, ServerCallContext context)
+        {
+            var command = _mapper.Map<CreateFeedbackCommand>(request);
+            var result = await _sender.Send(command);
+            var response = _mapper.Map<FeedbackModel>(result);
+
+            return response;
+        }
     }
 }
