25c66fc8110d6d471735bfa4e8068c16996efc05
andriikuklinov <akuklinov@outlook.com>
2025-06-21 17:20:03 +0300

    Implement FeedbackRepository.

---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Data/Repositories/Contracts/IFeedbackRepository.cs
@@ -0,0 +1,12 @@
+﻿using Feedback.API.Data.Entities;
+using FeedbackEntity = Feedback.API.Data.Entities.Feedback;
+
+namespace Feedback.API.Data.Repositories.Contracts
+{
+    public interface IFeedbackRepository
+    {
+        Task<FeedbackEntity> CreateFeedback(FeedbackEntity feedback);
+        Task<FeedbackEntity> UpdateFeedback(FeedbackEntity feedback);
+        Task<int> DeleteFeedback(FeedbackEntity feedback);
+    }
+}
---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Data/Repositories/FeedbackRepository.cs
@@ -0,0 +1,31 @@
+﻿using DevimaFeedbackSystem.Common.Core.DataAccess.Repository;
+using FeedbackEntity = Feedback.API.Data.Entities.Feedback;
+
+namespace Feedback.API.Data.Repositories
+{
+    public class FeedbackRepository: GenericRepository<FeedbackEntity, FeedbackContext>
+    {
+        public FeedbackRepository(FeedbackContext context):base(context)
+        {
+            
+        }
+
+        public async Task<Entities.Feedback> CreateFeedback(Entities.Feedback feedback)
+        {
+            var result = await AddAsync(feedback);
+            return result;
+        }
+
+        public async Task<int> DeleteFeedback(Entities.Feedback feedback)
+        {
+            int feedbackId = await RemoveAsync(feedback);
+            return feedbackId;
+        }
+
+        public async Task<Entities.Feedback> UpdateFeedback(Entities.Feedback feedback)
+        {
+            var result = await UpdateAsync(feedback);
+            return result;
+        }
+    }
+}
