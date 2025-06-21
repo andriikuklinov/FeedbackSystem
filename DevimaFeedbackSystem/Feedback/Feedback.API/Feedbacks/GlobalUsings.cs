8e24e8df5f3e6cf07e97d699c696cf0ed166077e
andriikuklinov <akuklinov@outlook.com>
2025-06-21 17:20:59 +0300

    Add feedback.proto .

---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Feedbacks/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿namespace Feedback.API.Feedbacks
+{
+    public class GlobalUsings
+    {
+    }
+}
---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Services/FeedbackService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Feedback.API.Data.Entities;
+using Feedback.API.Protos;
+
+namespace Feedback.API.Services
+{
+    public class FeedbackService: FeedbackProtoService.FeedbackProtoServiceBase
+    {
+        private readonly ILogger<FeedbackService> _logger;
+        private readonly IMapper _mapper;
+        private readonly IFeedbackService
+    }
+}
