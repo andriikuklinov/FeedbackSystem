0aac4e16ac4bec85bac333706c6b8f6410829aeb
andriikuklinov <akuklinov@outlook.com>
2025-06-24 16:08:59 +0300

    Implement GetUserFeedbacks query.

---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Feedbacks/Queries/GetUserFeedbacks/GetUserFeedbacksQuery.cs
@@ -0,0 +1,15 @@
+﻿using DevimaFeedbackSystem.Common.Core.CQRS;
+using Feedback.API.Feedbacks.Queries.Common;
+
+namespace Feedback.API.Feedbacks.Queries.GetUserFeedbacks
+{
+    public class GetUserFeedbacksQuery: IQuery<GetFeedbacksResult>
+    {
+        public int UserId { get; private set; }
+
+        public GetUserFeedbacksQuery(int userId)
+        {
+            UserId = userId;
+        }
+    }
+}
---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Feedbacks/Queries/GetUserFeedbacks/GetUserFeedbacksQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DevimaFeedbackSystem.Common.Core.CQRS;
+using Feedback.API.Data.Repositories.Contracts;
+using Feedback.API.Feedbacks.Queries.Common;
+
+namespace Feedback.API.Feedbacks.Queries.GetUserFeedbacks
+{
+    public class GetUserFeedbacksQueryHandler : IQueryHandler<GetUserFeedbacksQuery, GetFeedbacksResult>
+    {
+        private IFeedbackRepository _feedbackRepository;
+
+        public GetUserFeedbacksQueryHandler(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
+        public async Task<GetFeedbacksResult> Handle(GetUserFeedbacksQuery query, CancellationToken cancellationToken)
+        {
+            var result = await _feedbackRepository.GetUserFeedbacks(query.UserId);
+            return new GetFeedbacksResult(result);
+        }
+    }
+}
