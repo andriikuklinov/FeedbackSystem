4ff1d8ebc9be52b0e3c18af25edcb525653e3730
andriikuklinov <akuklinov@outlook.com>
2025-06-24 19:24:39 +0300

    Add mappings for getUserFeedbacksQuery and GetFeedbacksByModuleIdQuery.

--- DevimaFeedbackSystem/Feedback/Feedback.API/MappingProfile/FeedbackMappingProfile.cs
+++ DevimaFeedbackSystem/Feedback/Feedback.API/MappingProfile/FeedbackMappingProfile.cs
@@ -1,6 +1,13 @@
 ﻿using AutoMapper;
 using Feedback.API.Data.Repositories;
 using Feedback.API.Data.Repositories.Contracts;
+using Feedback.API.Feedbacks.Queries.Common;
+using Feedback.API.Feedbacks.Queries.GetFeedbacksByModuleId;
+using Feedback.API.Feedbacks.Queries.GetUserFeedbacks;
+using Feedback.API.Protos;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+using FeedbackEntity = Feedback.API.Data.Entities.Feedback;
 
 namespace Feedback.API.MappingProfile
 {
@@ -9,6 +16,12 @@ namespace Feedback.API.MappingProfile
         public FeedbackMappingProfile()
         {
             CreateMap<IFeedbackRepository, FeedbackRepository>();
+            CreateMap<GetFeedbackByUserIdRequest, GetUserFeedbacksQuery>();
+            CreateMap<GetFeedbacksResult, FeedbacksResponse>().ForMember(destination => destination.Feedbacks,
+                source => source.MapFrom(feedback => feedback.Feedbacks));
+            CreateMap<FeedbackEntity, FeedbackModel>()
+                .ForMember(destination=>destination.Comment, source=>source.MapFrom(_=>_.Comment ?? string.Empty));
+            CreateMap<GetFeedbacksByModuleIdRequest, GetFeedbacksByModuleIdQuery>();
         }
     }
 }
