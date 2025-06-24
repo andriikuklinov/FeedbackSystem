4ec813f58c7eb7d7eccdfa25fab2af216eaf3a89
andriikuklinov <akuklinov@outlook.com>
2025-06-24 16:10:54 +0300

    Automapper and MediatR configuration.

--- DevimaFeedbackSystem/Feedback/Feedback.API/Program.cs
+++ DevimaFeedbackSystem/Feedback/Feedback.API/Program.cs
@@ -1,13 +1,25 @@
 using Feedback.API.Data;
+using Feedback.API.Data.Repositories;
+using Feedback.API.Data.Repositories.Contracts;
+using Feedback.API.MappingProfile;
 using Feedback.API.Services;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
 builder.Services.AddDbContext<FeedbackContext>((options) =>
 {
     options.UseInMemoryDatabase("FeedbackDb");
 });
 builder.Services.AddGrpc();
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new FeedbackMappingProfile());
+});
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 app.MapGrpcService<FeedbackService>();
