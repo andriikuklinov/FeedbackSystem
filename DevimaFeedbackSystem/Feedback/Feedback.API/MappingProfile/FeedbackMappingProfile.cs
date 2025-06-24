fdb1eaf060d2ec77a1cd4725c586533ddccf918f
andriikuklinov <akuklinov@outlook.com>
2025-06-24 16:10:25 +0300

    Add mapping profile for Feedback service.

---
+++ DevimaFeedbackSystem/Feedback/Feedback.API/MappingProfile/FeedbackMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Feedback.API.Data.Repositories;
+using Feedback.API.Data.Repositories.Contracts;
+
+namespace Feedback.API.MappingProfile
+{
+    public class FeedbackMappingProfile: Profile
+    {
+        public FeedbackMappingProfile()
+        {
+            CreateMap<IFeedbackRepository, FeedbackRepository>();
+        }
+    }
+}
