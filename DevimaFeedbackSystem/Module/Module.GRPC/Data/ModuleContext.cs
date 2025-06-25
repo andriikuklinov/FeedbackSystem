bf1a9cb4793c4405a09db672efdc63b20fd06e79
andriikuklinov <akuklinov@outlook.com>
2025-06-25 11:33:10 +0300

    Implement ModuleRepository.

---
+++ DevimaFeedbackSystem/Module/Module.GRPC/Data/ModuleContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using ModuleEntity = Module.GRPC.Data.Entities.Module;
+
+namespace Module.GRPC.Data
+{
+    public class ModuleContext: DbContext
+    {
+        public DbSet<ModuleEntity> Modules { get; set; }
+
+        public ModuleContext(DbContextOptions<ModuleContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ModuleEntity>().HasKey(module => module.Id);
+            modelBuilder.Entity<ModuleEntity>().Property(module => module.Name).IsRequired();
+
+            modelBuilder.Entity<ModuleEntity>().HasData(new List<ModuleEntity>()
+            {
+                new ModuleEntity(){ Id=1, Name="First module" },
+                new ModuleEntity(){ Id=2, Name="Second module" },
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DevimaFeedbackSystem/Module/Module.GRPC/Data/Repositories/Contracts/IModuleRespository.cs
@@ -0,0 +1,9 @@
+﻿using ModuleEntity = Module.GRPC.Data.Entities.Module;
+
+namespace Module.GRPC.Data.Repositories.Contracts
+{
+    public interface IModuleRespository
+    {
+        Task<IEnumerable<ModuleEntity>> GetModules();
+    }
+}
