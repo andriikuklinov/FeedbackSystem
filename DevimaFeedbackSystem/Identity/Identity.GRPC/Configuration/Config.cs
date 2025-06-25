c614c0a667d68044a82740774eed9bff5c38e688
andriikuklinov <akuklinov@outlook.com>
2025-06-25 18:25:17 +0300

    Add basic configuration for Identity.GRPC project.

---
+++ DevimaFeedbackSystem/Identity/Identity.GRPC/Configuration/Config.cs
@@ -0,0 +1,30 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+
+namespace Identity.GRPC.Configuration
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients => new Client[] 
+        {
+
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+
+        };
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+
+        };
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+
+        };
+        public static IEnumerable<TestUser> TestUsers => new TestUser[]
+        {
+
+        };
+    }
+}
--- DevimaFeedbackSystem/Identity/Identity.GRPC/Program.cs
+++ DevimaFeedbackSystem/Identity/Identity.GRPC/Program.cs
@@ -1,6 +1,18 @@
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+using Identity.GRPC.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddTestUsers(Config.TestUsers.ToList())
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseIdentityServer();
 
 app.Run();
