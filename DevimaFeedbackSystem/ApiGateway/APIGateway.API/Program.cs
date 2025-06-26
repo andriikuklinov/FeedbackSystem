349e913e366156c8e0a9ac475ac0873f8cc5b3e5
andriikuklinov <akuklinov@outlook.com>
2025-06-26 19:21:58 +0300

    Secure gateway api.

--- DevimaFeedbackSystem/ApiGateway/APIGateway.API/Program.cs
+++ DevimaFeedbackSystem/ApiGateway/APIGateway.API/Program.cs
@@ -1,18 +1,32 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot();
+builder.Services.AddControllers();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:7258";
+        options.Audience = "gatewayResource";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "https://localhost:7258",
+            ValidAudience = "gatewayResource",
+            //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret"))
+        };
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-app.UseOcelot();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
